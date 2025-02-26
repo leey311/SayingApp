ef215171363c5e20f13ce7a3fcc576221b8d67d8
leey311 <wwyl1@naver.com>
2025-02-26 12:15:05 +0900

    테스트 데이터 생성

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,9 +4,16 @@ import com.ll.engine.App;
 import com.ll.engine.SayingAppFactory;
 
 public class Main {
+    static SayingAppFactory sayingAppFactory = new SayingAppFactory();
     public static void main(String[] args) {
-        SayingAppFactory sayingAppFactory = new SayingAppFactory();
+        intiData();
         App app = sayingAppFactory.createApp();
         app.run();
+    }
+    public static void intiData(){
+        for(int i = 0; i < 10; i++){
+            sayingAppFactory.createRepository()
+                    .registApp(String.format("작가%s", i), String.format("명언%s", i));
+        }
     }
 }
